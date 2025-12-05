b2b17708acf25f7ec266d1d00d043a8047eac338
bllry <83239039+bllryy@users.noreply.github.com>
2025-12-05 01:23:27 -0500

    Update solve.py

--- day5/solve.py
+++ day5/solve.py
@@ -1,19 +1,12 @@
 """
-we can just see if each number is in each range
-iterated over all ranges to check if the given 
-ingredient is part of it or not
-
-Read the whole file
-Split on the blank line
-Parse ranges and ingredients separately
-Count ingredients that fall in at least one range
-
-The database operates on ingredient IDs. It consists of a list of fresh ingredient ID ranges, a 
-blank line, and a list of available ingredient IDs. For example:
+parse
+counts how many of those integers fall inside any interval (a).
+merges the intervals in order and counts how many total integer positions they cover (b).
 
 https://stackoverflow.com/questions/11315010/what-do-and-before-a-variable-name-mean-in-a-function-signature
 https://www.reddit.com/r/learnpython/comments/dor6bu/how_does_the_star_before_argument_not_a_parameter/
 wicked pull
+https://www.w3schools.com/python/ref_func_sorted.asp
 """
 
 F, I = open('in.txt').read().split('\n\n')
